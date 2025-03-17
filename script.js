9a49e9bdef367d2f43876a2e4cd973dd3fdb71e9
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-03-17 15:49:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function toCase(text) {
   // write your code here
+	    if (text.length === 0) return "-";
+    return text.toLowerCase() + "-" + text.toUpperCase();
+
 }
 
 // DO not change the code below
